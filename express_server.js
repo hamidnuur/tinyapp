230a2b3105999ac04a9b1635fc8d795e5e67b7a1
Hamid Nur <hamidsharif2001@gmail.com>
2021-12-08 13:30:18 -0500

    save POST request to urlDatabase

--- express_server.js
+++ express_server.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL;
 });
 
 app.get("/", (req, res) => {
